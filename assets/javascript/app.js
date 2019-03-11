5ed2640e99d3c4226edf4bfb1f39cf12a8c61eb9
Oogapooki <andrewosborn101@gmail.com>
2019-03-11 00:14:51 -0500

    added basic layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+//create a start button underneath the title that loads the quiz and starts the timer
+//create 10 questions with 4 answers per question
+    //next to each answer, have a box to check that's linked with the other 3 boxes for the question
+    //if user chooses correctly, record true and place in "correct" variable, if incorrect, record false and store in "incorrect" variable
+//create a button at bottom of screen called submit
+    //when clicked, change content below title to a results page showing how many correct and incorrect answers.
